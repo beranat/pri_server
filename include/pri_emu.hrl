8daf3f112f08aa40b7bbd22db7720ae4831bd339
madRat <madrat@hotbox.ru>
2014-11-08 14:11:44 +0000

    fix names

--- include/pri_emu.hrl
+++ include/pri_emu.hrl
@@ -6,146 +6,146 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(EMU_SERVER,	'emu_server').
--define(EMU_CLIENT,	'emu_client').
+-define(PRI_SERVER_EMU_TABLE,	'pri_server_emu_server').
+-define(PRI_SERVER_EMU_CLIENT,	'pri_server_emu_client').
 
 %------------------------------------------------------------------------------
-emu_init(Args) ->
+pri_server_emu_init(Args) ->
 	ok = meck:new(pri_server),
 
-	ok = meck:expect(pri_server, call, fun emu_server_call/2),
-	ok = meck:expect(pri_server, call, fun emu_server_call/3),
-	ok = meck:expect(pri_server, call, fun emu_server_call/4),
+	ok = meck:expect(pri_server, call, fun pri_server_emu_server_call/2),
+	ok = meck:expect(pri_server, call, fun pri_server_emu_server_call/3),
+	ok = meck:expect(pri_server, call, fun pri_server_emu_server_call/4),
 
-	ok = meck:expect(pri_server, cast, fun emu_server_cast/2),
-	ok = meck:expect(pri_server, cast, fun emu_server_cast/3),
+	ok = meck:expect(pri_server, cast, fun pri_server_emu_server_cast/2),
+	ok = meck:expect(pri_server, cast, fun pri_server_emu_server_cast/3),
 
-	ok = meck:expect(pri_server, reply, fun emu_server_reply/2),
+	ok = meck:expect(pri_server, reply, fun pri_server_emu_server_reply/2),
 
-	State = emu_init([]),
-	ets:new(?EMU_SERVER, [named_table, set, private]),
+	State = pri_server_emu_init([]),
+	ets:new(?PRI_SERVER_EMU_TABLE, [named_table, set, private]),
 	Result = init(Args),
-	?assertNot(emu_has(reply)),
+	?assertNot(pri_server_emu_has(reply)),
 	?assertMatch({ok, _State}, Result),
 	{ok, State} = Result,
-	emu_set(state, State),
+	pri_server_emu_set(state, State),
 	State.
 
 %------------------------------------------------------------------------------
-emu_done() ->
-	?assertNot(emu_has(reply)),
-	State = emu_get(state),
-	terminate(State, 'emulation_completed'),
-	catch ets:delete(?EMU_SERVER),
+pri_server_emu_done() ->
+	?assertNot(pri_server_emu_has(reply)),
+	State = pri_server_emu_get(state),
+	terminate(State, 'pri_server_emulation_completed'),
+	catch ets:delete(?PRI_SERVER_EMU_TABLE),
 	ok = meck:unload(pri_server).
 
 %------------------------------------------------------------------------------
-emu_has(Param) ->
-	[] =/= ets:lookup(?EMU_SERVER, Param).
+pri_server_emu_has(Param) ->
+	[] =/= ets:lookup(?PRI_SERVER_EMU_TABLE, Param).
 
 %------------------------------------------------------------------------------
-emu_get(Param) ->
-	[{Param, Value}] = ets:lookup(?EMU_SERVER, Param),
+pri_server_emu_get(Param) ->
+	[{Param, Value}] = ets:lookup(?PRI_SERVER_EMU_TABLE, Param),
 	Value.
 
 %------------------------------------------------------------------------------
-emu_pop(Param) ->
-	?assert(emu_has(Param)),
-	Value = emu_get(Param),
-	emu_clear(Param),
+pri_server_emu_pop(Param) ->
+	?assert(pri_server_emu_has(Param)),
+	Value = pri_server_emu_get(Param),
+	pri_server_emu_clear(Param),
 	Value.
 
 %------------------------------------------------------------------------------
-emu_set(Param, Value) ->
-	emu_clear(Param),
-	ets:insert_new(?EMU_SERVER, {Param, Value}).
+pri_server_emu_set(Param, Value) ->
+	pri_server_emu_clear(Param),
+	ets:insert_new(?PRI_SERVER_EMU_TABLE, {Param, Value}).
 
 %------------------------------------------------------------------------------
-emu_clear(Param) ->
-	ets:delete(?EMU_SERVER, Param).
+pri_server_emu_clear(Param) ->
+	ets:delete(?PRI_SERVER_EMU_TABLE, Param).
 
 %------------------------------------------------------------------------------
-emu_server_reply(Client, Reply) ->
-	?assertEqual(?EMU_CLIENT, Client),
-	?assertNot(emu_has(reply)),
-	emu_set(reply, Reply).
+pri_server_emu_server_reply(Client, Reply) ->
+	?assertEqual(?PRI_SERVER_EMU_CLIENT, Client),
+	?assertNot(pri_server_emu_has(reply)),
+	pri_server_emu_set(reply, Reply).
 
 %------------------------------------------------------------------------------
-emu_server_call(ServerRef, Request) ->
-	emu_server_call(ServerRef, Request, 5000).
+pri_server_emu_server_call(ServerRef, Request) ->
+	pri_server_emu_server_call(ServerRef, Request, 5000).
 
-emu_server_call(ServerRef, Request, Timeout) ->
-	emu_server_call(ServerRef, Request, Timeout, normal).
+pri_server_emu_server_call(ServerRef, Request, Timeout) ->
+	pri_server_emu_server_call(ServerRef, Request, Timeout, normal).
 
-emu_server_call(ServerRef, Request, Timeout, Priority) ->
+pri_server_emu_server_call(ServerRef, Request, Timeout, Priority) ->
 	?assert((is_integer(Timeout) andalso Timeout>0) or (infinity == Timeout)),
 	?assert((low == Priority) or (normal == Priority) or (high == Priority)),
 	?assert(?MODULE == ServerRef), % supports yet ONLY local,
 
-	?assertNot(emu_has(reply)), % emu_clear(reply),
-	State = emu_get(state),
+	?assertNot(pri_server_emu_has(reply)), % pri_server_emu_clear(reply),
+	State = pri_server_emu_get(state),
 
-	{Reply1, State1} = case handle_call(Request, ?EMU_CLIENT, State) of
+	{Reply1, State1} = case handle_call(Request, ?PRI_SERVER_EMU_CLIENT, State) of
 		{reply, Reply, NewState} ->
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			{Reply, NewState};
 		{reply, Reply, NewState, Timeout} ->
 			?assert(is_integer(Timeout) and (Timeout>0)),
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			{Reply, NewState};
 		{reply,Reply,NewState,hibernate} ->
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			{Reply, NewState};
 		{noreply,NewState} ->
-			{emu_pop(reply), NewState};
+			{pri_server_emu_pop(reply), NewState};
 		{noreply,NewState,Timeout} ->
 			?assert(is_integer(Timeout) and (Timeout>0)),
-			{emu_pop(reply), NewState};
+			{pri_server_emu_pop(reply), NewState};
 		{noreply,NewState,hibernate} ->
-			{emu_pop(reply), NewState};
+			{pri_server_emu_pop(reply), NewState};
 		{stop,_Reason,Reply,NewState} ->
-			?debugFmt("Server emulation stopped in ~w with ~w", [?MODULE, _Reason]),
-			?assertNot(emu_has(reply)),
+			?debugFmt("Server pri_server_emulation stopped in ~w with ~w", [?MODULE, _Reason]),
+			?assertNot(pri_server_emu_has(reply)),
 			{Reply,NewState};
 		{stop,_Reason,NewState} ->
-			?debugFmt("Server emulation stopped in ~w with ~w", [?MODULE, _Reason]),
-			{emu_pop(reply), NewState};
+			?debugFmt("Server pri_server_emulation stopped in ~w with ~w", [?MODULE, _Reason]),
+			{pri_server_emu_pop(reply), NewState};
 		_Any ->
 			?assert(false)
 	end,
-	emu_set(state, State1),
+	pri_server_emu_set(state, State1),
 	Reply1.
 
 %------------------------------------------------------------------------------
-emu_server_cast(ServerRef, Request) ->
-	emu_server_cast(ServerRef, Request, normal).
+pri_server_emu_server_cast(ServerRef, Request) ->
+	pri_server_emu_server_cast(ServerRef, Request, normal).
 
-emu_server_cast(ServerRef, Request, Priority) ->
+pri_server_emu_server_cast(ServerRef, Request, Priority) ->
 	?assert((low == Priority) or (normal == Priority) or (high == Priority)),
 	?assert(?MODULE == ServerRef), % supports yet ONLY local,
 
-	?assertNot(emu_has(reply)), % emu_clear(reply),
-	State = emu_get(state),
+	?assertNot(pri_server_emu_has(reply)), % pri_server_emu_clear(reply),
+	State = pri_server_emu_get(state),
 
 	State1 = case handle_cast(Request, State) of
 		{noreply,NewState} ->
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			NewState;
 		{noreply,NewState,Timeout} when is_integer(Timeout) ->
 			?assert(is_integer(Timeout) and (Timeout>0)),
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			NewState;
 		{noreply,NewState,hibernate} ->
-			?assertNot(emu_has(reply)),
+			?assertNot(pri_server_emu_has(reply)),
 			NewState;
 		{stop,_Reason,NewState} ->
-			?debugFmt("Server emulation stopped in ~w with ~w", [?MODULE, _Reason]),
-			?assertNot(emu_has(reply)),
+			?debugFmt("Server pri_server_emulation stopped in ~w with ~w", [?MODULE, _Reason]),
+			?assertNot(pri_server_emu_has(reply)),
 			NewState;
 		_Any ->
 			?assert(false)
 	end,
-	emu_set(state, State1),
+	pri_server_emu_set(state, State1),
 	ok.
 
 -endif.
